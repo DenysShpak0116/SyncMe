a6ef2c7b5e7fe11dafe70d2ee679fb734552b6c1
DenysShpak0116 <shpakdenys46@gmail.com>
2024-06-07 21:24:56 +0300

    server added

--- client/server.js
+++ client/server.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
-
+const cors = require('cors');
 const app = express()
 
-app.use('/', serveStatic(path.join(__dirname, '/dist')))
+app.use('/', serveStatic(path.join(__dirname, '/dist'))).use(cors())
 
 const port = process.env.PORT || 8080
 app.listen(port)
